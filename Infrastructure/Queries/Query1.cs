fbf54a1f043f4166bc6bebdea70874ccd6cf2db9
Christian Cabrera <cabrera.ch27@gmail.com>
2025-04-02 13:28:13 -0300

    Add Template

---
+++ Infrastructure/Queries/Query1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Queries
+{
+    public class Query1
+    {
+    }
+}
